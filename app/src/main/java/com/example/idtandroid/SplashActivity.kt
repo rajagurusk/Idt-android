703e1215017db1d3bca2aa862d8cf8cef069447d
rajagurusk <rajagurusivakumar1@gmail.com>
2025-08-20 18:10:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/idtandroid/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.idtandroid
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_DURATION: Long = 3000 // 3 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_DURATION)
+    }
+}
