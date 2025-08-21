b44495bb2bd311e95bc03095e9783d347f8b60ec
rajagurusk <rajagurusivakumar1@gmail.com>
2025-08-21 12:59:10 +0530

    login

--- app/src/main/java/com/example/idtandroid/MainActivity.kt
+++ app/src/main/java/com/example/idtandroid/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.idtandroid
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // Your splash layout
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // After 2 seconds, launch LoginActivity and finish splash
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
